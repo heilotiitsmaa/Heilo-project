ccafd7d50d869a8c892cdee0b83d170d943dbae7
heilotiitsmaa <heilo.tiitsmaa@voco.ee>
2025-01-18 18:37:03 +0200

    first commit

---
+++ Product.js
@@ -0,0 +1,15 @@
+export class Product {
+    constructor(name, price, quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = this.category;
+    }
+    describe(){
+        return `tootekood:${this.id}, toode:${this.title}, hind:${this.price}€, kategooria:${this.category}`;
+        }
+        //staatiline meetod
+        static discountedPrice(a, b) { //a=price b=discountPercent
+            return a - (a*(b/100))
+        }
+}
